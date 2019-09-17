d00904c357eb1304145e5b5b20a20c22f95714cb
Vipin <vipins110@gmail.com>
2019-09-17 15:04:55 +0530

    Login + dashboard demo pages completed

--- money-save-angular/src/app/app.module.ts
+++ money-save-angular/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Guest/home/home.component';
+import { LogoComponent } from './Layouts/logo/logo.component';
+import { LoginComponent } from './User/login/login.component';
+import { DashboardComponent } from './User/dashboard/dashboard.component';
+import { NavbarComponent } from './Layouts/navbar/navbar.component';
+
+const routes:Routes = [
+	{ path : '', component : HomeComponent },
+	{ path : 'login', component : LoginComponent },
+  { path : 'dashboard', component : DashboardComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    LogoComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    NgbModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
